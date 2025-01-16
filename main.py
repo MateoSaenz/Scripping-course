c2fa92f18c6c563fc68799250b3044406b45de5f
Mateo Saenz Ortiz <msaenzo@unal.edu.co>
2025-01-16 22:59:36 +0100

    Second module

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import os
 
 def main():
     for root, directories, files in os.walk("C:/Users/mateo/OneDrive/Documentos/IMT NORD EUROPE/AÑO 2/Scripping course/project_1"):
-        print(f"Root:{root}")
-        print(f"directory: {directories}")
-        print(f"File: {files}")
+       # print(f"Root:{root}")
+       # print(f"directory: {directories}")
+       # print(f"File: {files}")
+        for _file in files:
+            absolute_path = os.path.join(root, _file)
+            print(f"File path: {absolute_path}")
 
 
 if __name__ == '__main__':
